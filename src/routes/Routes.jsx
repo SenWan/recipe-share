5023bbe6ad99a618ee45e7a5c8de18d34399eb45
U Sen Wan <senwan96@gmail.com>
2024-05-31 14:42:57 +0600

    pages added and routed

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
             {
                 index:true,
                 element: <Home></Home>,
+            },
+            {
+                path: "about",
+                element: <About></About>
+            },
+            {
+                path: "contact",
+                element: <Contact></Contact>
             }
         ]
       },
