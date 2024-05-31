b49b74d7f8b352cc1db1e54eb30908874e0b8d8a
U Sen Wan <senwan96@gmail.com>
2024-05-31 15:27:29 +0600

    login and register page added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,8 @@ import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const router = createBrowserRouter([
     {
@@ -20,7 +22,15 @@ const router = createBrowserRouter([
             {
                 path: "contact",
                 element: <Contact></Contact>
-            }
+            },
+           {
+            path: "login",
+            element: <Login></Login>,
+           },
+           {
+            path: "register",
+            element: <Register></Register>,
+           }
         ]
       },
 ])
