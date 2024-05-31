c68cd0301625dec004daced2571191dcaf76dcbf
U Sen Wan <senwan96@gmail.com>
2024-05-31 15:44:25 +0600

    banner added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
+import Banner from "../components/home/Banner";
+
 const Home = () => {
     return (
         <div>
-            <h1>Hello from Home</h1>
+            <Banner></Banner>
         </div>
     );
 };
